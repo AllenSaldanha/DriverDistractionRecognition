7e9e3a87cc4a71a5f11a6c8ccaa651b4272e6911
PrateekCh <prateekchaturvedi216@gmail.com>
2025-06-26 20:21:52 +0200

    model def

--- src/train.py
+++ src/train.py
@@ -1,6 +1,11 @@
-from dataset import DriverActivityDataset
+import torch
 import torchvision.transforms as T
 from torch.utils.data import DataLoader
+
+from dataset import DriverActivityDataset
+from models.I3D import I3D
+
+NUM_CLASSES = 12
 
 transform = T.Compose([
     T.Resize((224, 224)),
@@ -13,8 +18,12 @@ dataset = DriverActivityDataset(
     transform=transform
 )
 
-dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=6, shuffle=True)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = I3D(num_classes=NUM_CLASSES).to(device)
 
 for frame, labels in dataloader:
     # training step here
-    print(frame, labels)
+    # print(frame, labels)
+    pass

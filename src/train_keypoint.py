fe37562273bc828db8f0301b1ce48e4b1e62a731
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2025-06-30 16:48:15 +0200

    train, val split

--- src/train_keypoint.py
+++ src/train_keypoint.py
@@ -1,24 +1,34 @@
+import torch
 import argparse
 import logging
 
+from torch.utils.data import DataLoader, random_split
 from keypoint_dataset import DriverActivityKeypointDataset
-from torch.utils.data import DataLoader
 from utils.video_annotation_pairs import collect_video_annotation_pairs
 
 logging.basicConfig(filename='training.log', level=logging.INFO)
 
 def main(pairs, keypoints_folder):
+    EPOCHS = 6
+    
     dataset = DriverActivityKeypointDataset(
         keypoints_folder= keypoints_folder,
         video_annotation_pairs=pairs,
         num_frames=16  # Number of frames in each sequence (Like I3D)
     )
 
-    # Wrap in DataLoader
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+     # Split dataset into training and validation sets (80%-20%)
+    val_size = int(0.2 * len(dataset))
+    train_size = len(dataset) - val_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=12, shuffle=True, num_workers=30, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=12, shuffle=False, num_workers=30, pin_memory=True)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Test loop
-    for keypoints, labels in dataloader:
+    for keypoints, labels in train_loader:
         # keypoints.shape -> [B, num_of_frames, N, 17, 2] where N is person count 17 is COCO points
         # labels.shape -> [B, num_classes]
         print(keypoints.shape, labels.shape)
